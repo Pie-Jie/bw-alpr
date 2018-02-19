f35b7832205e045acab9e57871e895e50256168f
Pieter-Jan <pieter-jan@wecovers-Mac-mini-i7.local>
2018-02-19 17:05:01 +0100

    Implemented ALPR

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
-var express = require('express');
+const express = require('express');
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var OpenalprApi = require('openalpr_api');
 var app = express();
-var server = http.createServer(app).listen(3000);
-var io = require('socket.io')(server);
+const PORT = process.env.PORT || 3000;
+var server = http.createServer(app).listen(PORT);
+const io = require('socket.io')(server);
+var imageUrl;
+var settings = "";
+var state = {};
 
 app.use(express.static('./public'));
+app.use(bodyParser.json());
 
-io.on('connection', function(socket) {
-    //console.log("connected");
-    socket.emit('message', 'Welcome to ALPR');
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        //console.log('a user disconnected');
+    });
+
+    socket.emit('title', 'Hello_friend');
+
+    fs.readdir('./uploads', function (err, files) {
+
+        var api = new OpenalprApi.DefaultApi()
+        var secretKey = "sk_fa23a171b24ee5578f0eab95";
+        var country = "eu";
+        var opts = {
+            'recognizeVehicle': 0,
+            'state': "",
+            'returnImage': 0,
+            'topn': 1,
+            'prewarp': ""
+        };
+        files.forEach(function (fileName) {
+
+            var file = path.join(__dirname, '/uploads', fileName);
+            var stats = fs.statSync(file);
+            if (stats.isFile() && fileName !== ".DS_Store") {
+                //console.log(typeof stats);
+                fs.readFile(file, 'UTF-8', function (err, contents) {
+                    
+                    //imageUrl = new Buffer(file).toString('base64');
+                    //console.log(imageUrl);
+                    var callback = function (error, data, response) {
+                        if (error) {
+                            console.error(error);
+                        } else {
+                            console.dir(data);
+                        }
+                    };
+                    api.recognizeBytes(contents, secretKey, country, opts, callback);
+
+                });
+            }
+        });
+
+    });
+
 });
 
-console.log('Starting socket app - http://localhost:3000');
+
+console.log(`Listening on port ${PORT}`);
--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,8 @@
-var socket = io('http://localhost:3000');
-socket.on('disconnect', function() {
-    setTitle('Disconnected');
+
+$(document).ready(function () {
+    var socket = io();
+    socket.on('title', function (msg) {
+        $('.title').text(msg);
+    });
+    
 });
-
-socket.on('connected', function() {
-    setTitle('Connected to ALPR');
-});
-
-socket.on('message', function(message) {
-   setTitle(message); 
-});
-
-function setTitle(title) {
-    document.querySelector("h2").innerHTML = title;
-}
-
